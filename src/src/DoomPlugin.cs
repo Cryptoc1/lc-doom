ba022af175b5e0cda5ab369be219009b7612c1ae
Samuel Steele <steelesam72@gmail.com>
2023-12-10 17:38:33 -0500

    - added license - generate plugin info at compile-time (versionining in csproj)

--- src/src/DoomPlugin.cs
+++ src/src/DoomPlugin.cs
@@ -4,7 +4,7 @@ using BepInEx.Logging;
 namespace LethalCompany.Doom;
 
 [BepInPlugin(DoomPluginInfo.Identifier, DoomPluginInfo.Name, DoomPluginInfo.Version)]
-public sealed class DoomPlugin : BaseUnityPlugin, IDisposable
+public sealed partial class DoomPlugin : BaseUnityPlugin, IDisposable
 {
     public static DoomPlugin Value { get; private set; } = default!;
 
@@ -15,16 +15,9 @@ public sealed class DoomPlugin : BaseUnityPlugin, IDisposable
     {
         Value = this;
         harmony = Harmony.CreateAndPatchAll(
-           typeof(DoomPlugin).Assembly,
-           DoomPluginInfo.Identifier);
+            typeof(DoomPlugin).Assembly,
+            DoomPluginInfo.Identifier);
     }
 
     public void Dispose() => (harmony as IDisposable)?.Dispose();
-}
-
-public static class DoomPluginInfo
-{
-    public const string Identifier = "cryptoc1.lethalcompany.doom";
-    public const string Name = "LC-DOOM";
-    public const string Version = "0.0.0";
 }
